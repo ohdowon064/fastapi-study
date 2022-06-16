fe82ed91b6dd26b41e6013209dcc7dc40453faa2
Dowon Oh <dhehdnjs03@gmail.com>
2022-06-16 19:49:23 +0900

    Dependency

---
+++ Dependencies/first_steps.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Depends, Cookie
+
+app = FastAPI()
+
+async def query_extractor(q: str | None = None):
+    return q
+
+def query_or_cookie_extractor(
+    q: str = Depends(query_extractor), last_query: str | None = Cookie(default=None)
+):
+    if not q:
+        return last_query
+    return q
+
+@app.get("/items")
+async def read_items(query_or_default: str = Depends(query_or_cookie_extractor)):
+    return {"q_or_cookie": query_or_default}
